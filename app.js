c5d3154a02136c207bd094a2cb7b72f651901dc5
Lasya Kora <lasya.kora@gmail.com>
2020-07-06 10:13:38 +0530

    Added grunt and made some changes to authentication

--- app.js
+++ app.js
@@ -9,18 +9,27 @@ var initializePassport = require('./passport-config')
 var methodOverride = require('method-override')
 
 
-const users = [];
+var users = [];
+/*if (typeof(req.session.users) == 'undefined') {
+        req.session.users = [];
+        
+    }
+users = req.session.users;*/
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var d = new Date();
-var curr_date=d.getFullYear()+'-'+('0'+ (d.getMonth() + 1)).slice(-2)+'-'+('0'+ d.getDate()).slice(-2);
+
 app.use(flash());
 
 /* Using the sessions */
 app.use(session({
 	secret: 'lasyasecret',
-	resave: false,
-	saveUninitialized: false
+	resave: true,
+	saveUninitialized: true,
+	 cookie : {
+        maxAge: 1000* 60 * 60 *24 * 365
+    },
+	rolling: true
 }))
 
 app.use(passport.initialize())
@@ -32,9 +41,9 @@ app.use(express.static(__dirname + '/public'));
 /* If there is no to do list in the session, 
 we create an empty one in the form of an array before continuing */
 app.use(function(req, res,next){
-    if (typeof(req.session.todolist) == 'undefined') {
-        req.session.todolist = [];
-        req.session.datelist = [];
+	//console.log('Use 1');
+	if (typeof(req.session.todo) == 'undefined') {
+        req.session.todo = [];
     }
     next();
 })
@@ -83,61 +92,65 @@ app.delete('/logout', (req,res)=> {
 })
 
 function checkAuthenticated(req,res,next){
+	//console.log('Check authenticated')
 	if(req.isAuthenticated()){
 		return next()
 	}
-	res.redirect('/login')
+	next();
 }
 
 function checkNotAuthenticated(req,res,next){
+	//console.log('Check not authenticated')
 	if(req.isAuthenticated()){
 		return res.redirect('/todo')
 	}
-	next()
+	next();
 }
 
 /* The to do list and the form are displayed */
 app.get('/todo',checkAuthenticated, function(req, res) { 
-    res.render('todo.ejs', {todolist: req.session.todolist, datelist: req.session.datelist, search_value: ''});
+    res.render('todo.ejs', {todolist: req.session.todo, search_value: ''});
 })
 
 /* Adding an item to the to do list */
 app.post('/todo/add/', function(req, res) {
-	let date= new Date();
-    let myDate= curr_date;
+	var d = new Date();
+	var myDate=d.getFullYear()+'-'+('0'+ (d.getMonth() + 1)).slice(-2)+'-'+('0'+ d.getDate()).slice(-2);
     if(req.body.newdate != ''){
     	myDate=req.body.newdate;
     }
     if (req.body.newtodo != '') {
-    	req.session.todolist.push(req.body.newtodo);
-        req.session.datelist.push(myDate);
+    	req.session.todo.push({
+    	todoText:req.body.newtodo,
+    	dueDate: myDate});
     }
     res.redirect('/todo');
 })
 
+/* Searching an iten from the to do list*/
 app.post('/todo/search/',function(req,res){
 	let tlist=[];
-	let dlist=[];
-	for(let i=0;i<req.session.todolist.length;i++){
-		if(req.session.todolist[i].toUpperCase().indexOf(req.body.search_box.toUpperCase())!=-1){
-			req.session.tlist.push(req.session.todolist[i]);
-			req.session.dlist.push(req.session.datelist[i]);
+	for(let i=0;i<req.session.todo.length;i++){
+		if(req.session.todo[i].todoText.toUpperCase().indexOf(req.body.search_box.toUpperCase())!=-1){
+			tlist.push({
+				todoText: req.session.todo[i].todoText,
+				dueDate: req.session.todo[i].dueDate});
 		}
 	}
-	res.render('todo.ejs', {todolist: tlist, datelist: dlist, search_value: req.body.search_box});
+	res.render('todo.ejs', {todolist: tlist, search_value: req.body.search_box});
 })
 
 /* Deletes an item from the to do list */
 app.get('/todo/delete/:id', function(req, res) {
     if (req.params.id != '') {
-        req.session.todolist.splice(req.params.id, 1);
-        req.session.datelist.splice(req.params.id, 1);
+        req.session.todo.splice(req.params.id, 1);
     }
     res.redirect('/todo');
 })
 
 /* Redirects to the to do list if the page requested is not found */
 app.use(function(req, res,next){
+	//console.log('Use 2');
     res.redirect('/login');
 })
 
